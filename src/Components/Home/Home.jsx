9eea8e1d56c9e2b7205dc6a849339f7759569ca3
unknown <hema68802@gmail.com>
2024-07-21 17:04:44 +0300

    sec commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -42,6 +42,7 @@ const Home = () =>
   <div className="card-body">
     <h5 className="card-title">{item.title.split(' ').slice(0, 3).join(' ')}</h5>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <h5>{item.price}$</h5>
     <button onClick={()=>
       {
         Addprodect({"id":item.id,"imgsrc":item.imgsrc,"title":item.title.split(' ').slice(0, 3).join(' '),"Price":item.price,"Quentity":1 })
